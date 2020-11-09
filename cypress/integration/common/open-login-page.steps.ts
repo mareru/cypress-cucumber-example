5c49a82962871be505da57a325d20811a9214759
MarijanaRukavina <marijana.rukavina@ecx.io>
2020-11-09 23:36:26 +0100

    Start restructure of cucumber and pages

--- cypress/integration/common/open-login-page.steps.ts
+++ cypress/integration/common/open-login-page.steps.ts
@@ -1,16 +1,16 @@
-import {HomePage} from "../../pages/homePage";
-import { Given, When } from "cypress-cucumber-preprocessor/steps";
-import {LoginPage} from "../../pages/loginPage";
-import testData from "../../fixtures/testData";
+import { HomePage } from '../../pages/homePage';
+import { Given, When } from 'cypress-cucumber-preprocessor/steps';
+import { LoginPage } from '../login-tests/Login/loginPage';
+import testData from '../../fixtures/testData';
 
 let homePage = new HomePage();
 let loginPage = new LoginPage();
 
 Given(/^I visit home page$/, function () {
-    homePage.visit((<any>testData).baseUrl, (<any>testData).phoneNumber);
+  homePage.visit((<any>testData).baseUrl, (<any>testData).phoneNumber);
 });
 
 When(/^I navigate to login page$/, function () {
-    homePage.clickSignInButton();
-    loginPage.verify();
+  homePage.clickSignInButton();
+  loginPage.verify();
 });
--- cypress/integration/login-tests/Login/submit-login.steps.ts
+++ cypress/integration/login-tests/Login/submit-login.steps.ts
@@ -1,25 +1,25 @@
-import { Then, Given, And} from 'cypress-cucumber-preprocessor/steps';
-import {LoginPage} from "../../../pages/loginPage";
-import {DashboardPage} from "../../../pages/dashboardPage";
-import testData from "../../../fixtures/testData";
+import { Then, Given, And } from 'cypress-cucumber-preprocessor/steps';
+import { LoginPage } from './loginPage';
+import { DashboardPage } from '../../../pages/dashboardPage';
+import testData from '../../../fixtures/testData';
 
 let loginPage = new LoginPage();
 let dashboardPage = new DashboardPage();
 
 And(/^I enter valid credentials$/, () => {
-    loginPage.enterCredentials((<any>testData).email, (<any>testData).validPassword);
-    loginPage.clickLoginButton();
+  loginPage.enterCredentials((<any>testData).email, (<any>testData).validPassword);
+  loginPage.clickLoginButton();
 });
 
 Then(/^I can see my username displayed on the page$/, function () {
-    dashboardPage.verify((<any>testData).loggedInUser);
+  dashboardPage.verify((<any>testData).loggedInUser);
 });
 
 Given(/^I enter invalid credentials$/, () => {
-    loginPage.enterCredentials((<any>testData).email, (<any>testData).invalidPassword);
-    loginPage.clickLoginButton();
+  loginPage.enterCredentials((<any>testData).email, (<any>testData).invalidPassword);
+  loginPage.clickLoginButton();
 });
 
 Then(/^I see error message on the page$/, function () {
-    dashboardPage.checkInvalidPasswordErrorMessage((<any>testData).invalidPasswordErrorMessage);
+  dashboardPage.checkInvalidPasswordErrorMessage((<any>testData).invalidPasswordErrorMessage);
 });
